4acfc9610ed1f37b74dcf0302fc1720806e99382
FernandoBersellini <fernandobersellini@outlook.com.br>
2025-04-15 10:41:22 -0300

    :tada: Primeiro commit!

---
+++ src/main/java/com/diet_info/dietInfo/DietInfoApplication.java
@@ -0,0 +1,13 @@
+package com.diet_info.dietInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diet_info/dietInfo/DietInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diet_info.dietInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
